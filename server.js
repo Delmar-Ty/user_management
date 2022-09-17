7490d81cafb441873b4405c508a8dd7569097ce1
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2022-09-16 21:12:52 -0700

    Did more stuff

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Listening on port ${port}`);
+});
