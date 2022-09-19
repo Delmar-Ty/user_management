b6d62c33c839f1761299ee2d7c5f1c0722cd6a4e
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2022-09-19 11:34:42 -0700

    Did more stuff
    ok...

---
+++ public/assets/JavaScript/login.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+const user = document.querySelector('#user');
+const form = document.querySelector('.form');
+
+btn.addEventListener('click', () => {
+    if (user.value.length > 16) {
+        
+    }
+});
