feda4a4ce6debd32c833cc864cd25cf247dcdaa1
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2022-09-20 11:52:56 -0700

    Trying stuff?
    Trying to get form validation to work

--- public/assets/JavaScript/signup.js
+++ public/assets/JavaScript/signup.js
@@ -1,14 +1,12 @@
 const form = document.querySelector('.form');
 const zip = document.querySelector('#zip');
 const btn = document.querySelector('.btn');
-const regZip = /\d{5}/;
 
+const inputs = document.querySelectorAll('input');
+const errors = [];
 
 btn.addEventListener('click', () => {
-    let input = zip.value;
-    if (input.match(regZip)) {
-        console.log('Match');
-        return;
-    }
-    console.log('No Match');
+    inputs.forEach((el) => {
+        console.log(el);
+    });
 });
