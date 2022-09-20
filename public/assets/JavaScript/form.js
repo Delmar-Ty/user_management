21e4a95a1845b6c286ba379efa686c31bfd1f0b3
Delmar <reliablesource13@gmail.com>
2022-09-20 15:26:29 -0700

    haha I am insane ;)
    definitely mentally stable

---
+++ public/assets/JavaScript/form.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.form');
+const zip = document.querySelector('#zip');
+const btn = document.querySelector('.btn');
+const pass = document.querySelectorAll('input[type="password"]');
+
+const inputs = document.querySelectorAll('input');
+
+btn.addEventListener('click', () => {
+    let errCount = 0;
+    btn.setAttribute('style', '--msg: "";');
+    inputs.forEach((el) => {
+        if (!el.value) {
+            errCount++;
+        }
+    });
+    if (pass[0].value !== pass[1].value) {
+        btn.setAttribute('style', '--msg: "Passwords Do Not Match";');
+        errCount++;
+    } else if (zip.value.length !== 5 && errCount <= 1) {
+        btn.setAttribute('style', '--msg: "Invalid Zip Code";');
+        errCount++;
+    } else if (errCount > 0) { 
+        btn.setAttribute('style', '--msg: "Please Enter All Fields Correctly";'); 
+    }
+});
